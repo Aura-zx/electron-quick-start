7636786350583b1e8971859633f1f1ad74918358
Aura-zx <kaomygod@gmail.com>
2019-04-18 15:43:00 +0800

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,3 +1,30 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+console.log('hello world!')
+var ipc = require('electron').ipcRenderer;
+const currentWindow = require('electron').remote.getCurrentWindow()
+/*document.body.addEventListener('click', () => {
+  console.log('hello vscode!')
+  
+})*/
+document.getElementById('maxbt').addEventListener('click', () => {
+  console.log('hello vscode!')
+  // ipc.send('window-max');
+  if (currentWindow.isMaximized()) {
+    currentWindow.unmaximize();
+  } else {
+      currentWindow.maximize();
+  }
+})
+document.getElementById('minbt').addEventListener('click', () => {
+  console.log('hello vscode!')
+  // ipc.send('window-min');
+  ipc.send('window-max')
+  
+})
+document.getElementById('closebt').addEventListener('click', () => {
+  console.log('hello vscode!')
+  ipc.send('window-close');
+  
+})
